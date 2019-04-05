5abda447124526609a3bc08150d46224b2b921ad
Reese Lee <reese.lee.cy@gmail.com>
2019-04-05 10:11:27 -0700

    add a for loop to business logic to make sure input form and initial variables are working

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,31 @@ var beepThree = "I'm sorry, Dave. I'm afraid I can't do that."
 
 
 function beepBoop(number) {
-  var beepBoopArray = "";
-
+  var beepBoopArray = [];
+  var numberInput = parseInt($("#integer-input").val());
   for (var i = 0; i < 100; i++) {
-    var
-    beepBoopArray.push();
+    if (numberInput === 1) {
+      return beepOne;
+    } else if (numberInput === 2) {
+      return beepTwo;
+    } else if (numberInput === 3) {
+        return beepThree;
+    } else {
+        return "Just kidding, Jenkins"
+    }
   }
-}
+    beepBoopArray.push();
+  };
+
+
+
 
 
 //user interface logic
 $(function() {
-  $(form#converter).submit(function(e) {
+  $("form#converter").submit(function(event) {
     event.preventDefault();
-    var numberInput = parseInt("#integer-input").val());
+    var numberInput = parseInt($("#integer-input").val());
     var result = beepBoop(numberInput);
     $("#result").show(result);
     $(".beepBoopNumber").text(result);
